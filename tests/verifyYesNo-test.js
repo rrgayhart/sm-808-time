690035965ee830edcdf5e9806577638586aae679
rrgayhart <rrgayhart@gmail.com>
2017-08-20 15:23:36 -0700

    Add no/yes verification

---
+++ tests/verifyYesNo-test.js
@@ -0,0 +1,32 @@
+var expect = require('chai').expect;
+var verifyYesNo = require('../commandline/verifyYesNo.js');
+
+describe('verifyYesNo', function () {
+  it('should return true for a yes or yes-like answer', function () {
+    var possibleYesResponses = [
+      'y',
+      'Y',
+      'yes',
+      'YES',
+      'YeS',
+      'Y '
+    ]
+    possibleYesResponses.forEach(function(resp){
+      expect(verifyYesNo(resp)).to.eq(true, ('Failed on ' + resp));
+    });
+  });
+
+  it('should return false for a no or no-like response', function(){
+    var possibleNoResponses = [
+      'n',
+      'N',
+      'no',
+      'asdfsesd',
+      'y a x',
+      'noyes'
+    ]
+    possibleNoResponses.forEach(function(resp){
+      expect(verifyYesNo(resp)).to.eq(false, ('Failed on ' + resp));
+    });
+  });
+});
